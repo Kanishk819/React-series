5ca78cbf341e63279e192219219019e5ebac0585
Kanishk819 <109857164+Kanishk819@users.noreply.github.com>
2024-01-21 10:43:59 +0530

    Hooks in React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,49 @@
 import Chai from "./chai"
+import { useState } from "react";
 function App() {
+
+
+  let [counter, setCounter ] = useState(15);
+
+  const addValue = ()=>{
+    console.log("clicked",counter);
+    if(counter < 20 )
+    setCounter(counter+1);
+    else 
+    setCounter(counter);
+  }
+  const removeValue = ()=>{
+    console.log("clicked",counter);
+    if(counter > 0 )
+    setCounter(counter-1);
+    else 
+    setCounter(counter);
+
+  }
   return (
     <>
     <Chai />
-    <h1>chai aur react</h1>
     <p>test para</p>
+    {/* counter = 15 */}
+
+    <h1>Counter Value : {counter}</h1>
+    <button onClick={addValue}>
+      Add Value {counter}
+    </button>
+    <br/>
+    <br/>
+    <br/>
+    <button onClick={removeValue}>     
+      Remove Value {counter}
+    </button>
     </>
   )
 }
 
 export default App
+
+
+/** UI mein ek saath bahut saari jagah par update Hooks ke help se kiya jaa sakta hai 
+ * let [counter, setCounter ] = useState(15);
+ * yahan pe React ek feature deta hai ki hooks se sarre jagah pe changes kiye jaa sakte hai
+*/
